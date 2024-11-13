4efba9d1399df71e20f3bf28ce59622537079d77
ethanschafstall <ethans9999@gmail.com>
2024-11-13 15:39:54 +0100

    feat(vite config): updates server config
    Adds hot module replacement to server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,6 +13,10 @@ export default defineConfig({
     }
   },
   server: {
+    hmr:{
+      host: "0.0.0.0",
+      port: 5173
+    },
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:3000',
